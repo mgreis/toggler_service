d131416bbbb1b76d4e4a681ee2a285a8defffdcb
Mario Pereira <mario.pereira@feedzai.com>
2018-05-22 00:40:26 +0100

    day2

--- Makefile
+++ Makefile
@@ -1,9 +1,12 @@
-.PHONY: install site docker-install docker-start docker-stop docker-restart
+.PHONY: install install-tests site docker-install docker-start docker-stop docker-restart
 
 VERSION=$(shell mvn -q -Dexec.executable='echo' -Dexec.args='$${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:exec)
 
 install:
 	mvn clean install
+
+install-tests:
+	mvn clean install -DskipTests=false
 
 site:
 	mvn clean install \
--- delivery/ff4j-springboot/src/main/java/com/mgreis/delivery/rsc/HomeResource.java
+++ delivery/ff4j-springboot/src/main/java/com/mgreis/delivery/rsc/HomeResource.java
@@ -15,15 +15,19 @@ public class HomeResource {
     @RequestMapping(value = "/", method = RequestMethod.GET, produces = "text/html")
     public String sayHello() {
         StringBuilder response = new StringBuilder("<html><body><ul>");
-        response.append("<li> To access the <b>WebConsole</b> please go to <a href=\"./ff4j-web-console/home\">ff4j-web-console</a>");
+        response.append(
+            "<li> To access the <b>WebConsole</b> please go to <a href=\"./ff4j-web-console/home\">ff4j-web-console</a>");
         response.append("<li> To access the <b>REST API</b> please go to <a href=\"./api/ff4j\">api/ff4j</a>");
-        response.append("<li> To access the <b>Swagger File </b> please go to <a href=\"./v2/api-docs\">/v2/api-docs</a>");
-        response.append("<li> To access the <b>Swagger -ZUI </b> please go to <a href=\"./swagger-ui.html\">/swagger-ui.html</a></ul>");
-        
-        response.append("<p>Is <span style=\"color:red\">Awesome</span> feature activated ? from  ff4j.check(\"AwesomeFeature\") <span style=\"color:blue\">");
+        response.append(
+            "<li> To access the <b>Swagger File </b> please go to <a href=\"./v2/api-docs\">/v2/api-docs</a>");
+        response.append(
+            "<li> To access the <b>Swagger -ZUI </b> please go to <a href=\"./swagger-ui.html\">/swagger-ui.html</a></ul>");
+
+        response.append(
+            "<p>Is <span style=\"color:red\">Awesome</span> feature activated ? from  ff4j.check(\"AwesomeFeature\") <span style=\"color:blue\">");
         response.append(ff4j.check("AwesomeFeature"));
         response.append("</span></body></html>");
+
         return response.toString();
     }
-
 }
